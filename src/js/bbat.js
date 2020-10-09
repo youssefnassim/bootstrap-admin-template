df37ef67def8604d0906d16ac5f25503330f4810
Youssef Nassim <ynassim7@gmail.com>
2020-10-09 16:21:28 +0100

    More refactoring and 1st Chart sample

--- src/js/bbat.js
+++ src/js/bbat.js
@@ -4,6 +4,8 @@
  *
  **/
 import Sidebar from './sidebar'
+// eslint-disable-next-line no-unused-vars
+import Chart from 'chart.js'
 
 import 'bootstrap'
 // eslint-disable-next-line no-unused-vars
@@ -16,3 +18,28 @@ window.addEventListener('scroll', function () {
     document.querySelector('.main header').classList.remove('fixed')
 })
 
+// chart
+// const ctx = document.getElementById('s').getContext('2d')
+
+// const testChart = new Chart(ctx, {
+//   type: 'line',
+//   options: {
+//     scales: {
+//       yAxes: [{
+//         ticks: {
+//           // eslint-disable-next-line object-shorthand
+//           callback: function (value) {
+//             return '$' + value + 'k'
+//           }
+//         }
+//       }]
+//     }
+//   },
+//   data: {
+//     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+//     datasets: [{
+//       label: 'Earned',
+//       data: [0, 10, 5, 15, 10, 20, 15, 25, 20, 30, 25, 40]
+//     }]
+//   }
+// })
